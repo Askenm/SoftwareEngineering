da84c8fa92f629eb64f692ab257a668875771ca6
Karl Kieler <karl@Karls-MacBook-Air-2.local>
2024-01-24 17:08:50 +0100

    skeleton of website, fixed some login bugs

--- Website/Login_signup.py
+++ Website/Login_signup.py
@@ -1,26 +1,12 @@
 # Front_page.py
-from pathlib import Path
 import streamlit as st
-from Authenticator_role.streamlit_authenticator import Authenticate
-import yaml
 
 
-def show():
+
+def show(authenticator):
     st.title("Login/Sign up")
-
-    config_path = Path(__file__).parent / "config.yaml"
-    with open(config_path) as file:
-        config = yaml.safe_load(file)
-
-    # Initialize authenticator with the configuration
-    authenticator = Authenticate(
-        config['credentials'],
-        config['cookie']['name'],
-        config['cookie']['key'],
-        config['cookie']['expiry_days']
-    )
+    username, name = None,None
     
-    authenticator.logout("Logout", "sidebar")
     # Toggle buttons for login and register
     if 'show_login' not in st.session_state:
         st.session_state['show_login'] = False
@@ -40,11 +26,6 @@ def show():
     if st.session_state['show_login']:
         name, authentication_status, username = authenticator.login("main")
         st.session_state['login_status'] = authentication_status
-        if authentication_status:
-            # Fetch and store the user's role upon successful login
-            user_info = config['credentials']['usernames'].get(username, {})
-            st.session_state['role'] = user_info.get('role', None)  # Default to None if role is not defined
-            st.sidebar.title(f"Welcome {name}, {st.session_state['role']}")
         if authentication_status == False:
             st.error("Username/password is incorrect")
 
@@ -58,3 +39,5 @@ def show():
                 st.session_state['show_login'] = True
         except Exception as e:
             st.error(e)
+
+    return authenticator, username, name
--- Website/app.py
+++ Website/app.py
@@ -1,8 +1,9 @@
 import streamlit as st
-import Front_page
-import Tournament_page
-import Battle_page
-import Create_battle
+import Login_signup as Login_signup
+import My_tournaments as My_tournaments, My_battles as My_battles, Create_battle, Home as Home, Submissions as Submissions, Create_tournament, Create_badge
+import yaml
+from pathlib import Path
+from Authenticator_role.streamlit_authenticator import Authenticate
 
 # Set up the main configuration of the app
 st.set_page_config(page_title="CodeKata Battles", page_icon="CBK")
@@ -12,34 +13,64 @@ if 'login_status' not in st.session_state:
 
 # Define the page navigation
 pages = {
-    "Login/Sign up": Front_page,
-    "Tournament": Tournament_page,
-    "Battle": Battle_page
+    #"Login/Sign up": Login_signup,
+    "Tournaments": My_tournaments,
+    "Battle page": My_battles,
+    "User profile": Home,
+    
+
 }
 student_pages = {
+    "Submission log": Submissions,
 }
-educator_pages = { "Create Battle": Create_battle,
+educator_pages = {"Create Battle": Create_battle,
+                  "Create Tournament": Create_tournament, 
+                  "Create Badge": Create_badge,
 }
 
+config_path = Path(__file__).parent / "config.yaml"
+with open(config_path) as file:
+    config = yaml.safe_load(file)
+
+# Initialize authenticator with the configuration
+authenticator = Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days']
+)
 # Function to show appropriate pages based on role
 def show_pages_based_on_role():
     if st.session_state['role'] == "Educator":
-        combined_pages = {**pages, **educator_pages}
-        page = st.sidebar.radio("Select your page", list(combined_pages))
-        print(combined_pages)
-        combined_pages[page].show()
+        #combined_pages = {**pages, **educator_pages}
+        page = st.sidebar.radio("Select your page", list(pages))
+        pages[page].show()
     elif st.session_state['role'] == "Student":
         combined_pages = {**pages, **student_pages}
         page = st.sidebar.radio("Select your page", list(combined_pages))
         combined_pages[page].show()
 
 # Display the front page by default or when not logged in
-if not st.session_state['login_status']:
-    pages["Login/Sign up"].show()
+if not st.session_state['login_status'] or st.session_state['logout']:
+    st.session_state['logout'] = False
+    # print('pis')
+    # print(f"{st.session_state['login_status']=}")
+    # print(f"{st.session_state['logout']=}\n\n")
+    authenticator, username, name = Login_signup.show(authenticator)
+    # Fetch and store the user's role upon successful login
+    user_info = config['credentials']['usernames'].get(username, {})
 
-
+    st.session_state['role'] = user_info.get('role', None)  # Default to None if role is not defined
+    st.session_state['name'] = name
 # Show other pages if logged in
 elif st.session_state['login_status']:
+    # print('prut')
+    # print(f"{st.session_state['login_status']=}")
+    # print(f"{st.session_state['logout']=}\n\n")
+    st.sidebar.title(f"Welcome {st.session_state['name']}, {st.session_state['role']}")
     show_pages_based_on_role()
+
+    authenticator.logout("Logout","sidebar")
+
 else:
     st.error("Please log in to access this page")
